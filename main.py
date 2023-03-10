80099cab796feb5cf08c23ba62ab730d0aa08b85
Joon <33235661+joonseoan@users.noreply.github.com>
2023-03-10 03:26:01 +0000

    added String manipulation

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 # Write your code below this line 👇
 
+# 0) print
+# (speace)  print("Hello World!") ===> IndentaionError
+
 # 1) String
-print('Hello World!'); # Double quotes
-print("Hello World!" + ' in Python'); # Single quotes
+print("Hello World!") # Double quotes
+print("Hello World!" + ' in Python') # Single quotes
+print("Hello World!" + " in Python") # + operator
+print("Hello World!", " in Python") # comma ==> another space
+print("Hello World!"" in Python") # no space
+print("Hello World!" " in Python") # Single quotes
+
+# 2) Manipulation
+# (1) next line
+print("hello\nhello\nhello")
+# (2) String Concatination
+print("Hello", "Angela")
+print("Hello" + " " + "Angela")
+print("Hello" + " Angela")
+
+
+
+
+
+
+# should start the lesson 7.
 
 
