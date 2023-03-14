0dd6cd8c249402cf6f11f43a1bc8bcbdc31c608b
Joon <33235661+joonseoan@users.noreply.github.com>
2023-03-14 02:33:36 +0000

    Added input function

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # Write your code below this line 👇
 
-# 0) print
-# (speace)  print("Hello World!") ===> IndentaionError
+# ------------------- print function -------------------------
+
+# 0) space (indentation) error
+# (space)  print("Hello World!") ===> IndentaionError
 
 # 1) String
-print("Hello World!") # Double quotes
-print("Hello World!" + ' in Python') # Single quotes
-print("Hello World!" + " in Python") # + operator
-print("Hello World!", " in Python") # comma ==> another space
-print("Hello World!"" in Python") # no space
-print("Hello World!" " in Python") # Single quotes
+print("Hello World!")  # Double quotes
+print("Hello World!" + ' in Python')  # Single quote
+print("Hello World!" + " in Python")  # + operator
+print("Hello World!", " in Python")  # comma ==> another space
+print("Hello World!"
+      " in Python")  # no space
+print("Hello World!"
+      ' in Python')  # Single quote
 
 # 2) Manipulation
 # (1) next line
@@ -19,11 +23,13 @@ print("Hello", "Angela")
 print("Hello" + " " + "Angela")
 print("Hello" + " Angela")
 
-
-
-
-
-
-# should start the lesson 7.
-
-
+# -------------------- input function ------------------------
+# Usage: when we need to give some data after `print`
+# The user can enter some data from the console.
+# After the user enters some data, the input function is replaced with the user input.
+input("What is your name?")
+print("Hello " + input("What is the programmer name?"))
+# Get length of string (It start with index 1, not 0)
+# Also must think about the space. The space should be relfected for the start poistion
+# of the returned value.
+print( len( input("What is your name? ") ) )
