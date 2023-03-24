c7eacbabd88a7dbd10dc4dcbf10718db77336bf3
Joon <33235661+joonseoan@users.noreply.github.com>
2023-03-24 04:15:40 +0000

    Done for project

--- main.py
+++ main.py
@@ -1,16 +1,70 @@
 # [ ------------------------ Day2 ----------------------------------- ]
 
+# ======= Project ===========
+#If the bill was $150.00, split between 5 people, with 12% tip.
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+print("Welcome to the tip calculator.")
+total_bill = float(input("What was the total bill? $"))
+tip_percent = int(
+    input("What percentage tip would you like to give? 10, 12, or 15? "))
+num_persons = int(input("How many people to split the bill? "))
+pay_per_person = (total_bill * (1 + tip_percent / 100)) / num_persons
+pay_per_person = "{:.2f}".format(pay_per_person)
+
+print(f"Each person should pay: ${pay_per_person}")
+
+# two decimal number does not always work in terms of even 00, 50 etc.
+# In this case, it print like 22 or 22.5 (not 22.00 or 22.50)
+# print(f"Each person should pay: ${round(pay_per_person, 2)}")
+
 # ======== Mathmatical operator ====
+print("======== Number Manipulation ===========")
+8 / 3  # 2.6666666666666
+print(
+    int(8 / 3)
+)  # [IMPORTANT] 2 because it chops off the decimal numbers without any rounding!
+print(round(8 / 3))  # 3 because of rounding
+print(round(8 / 3, 2))  # 2.67 because 2 is the number of decimal numbers
+
+# directly converting float to integer
+print(8 // 3)  # 2 because it chops off the all decimal numbers
+print(type(8 // 3))  # int class
+
+# chaining variables
+result = 4 / 2
+print(result)  # 2.0
+
+# It is same as 'result = result / 2'
+# we can use *=, /=, +=, -=
+result /= 2
+print(result)  # 1.0
+
+# f-String: chaning all types of value to string.
+# We do not need to use str() for casting
+score = 0
+height = 1.9
+isWinning = True
+print(
+    f"Your score is {score}, your height is {height}, and your are winning is {isWinning}"
+)  # Same thing in Java and javascript
+
 print("======== Math operator ===========")
 3 + 5
 7 - 4
 3 * 2
 6 / 3
-print(6 / 3) # [IMPORTANT] it always returns float type! -> 2.0
+print(6 / 3)  # [IMPORTANT] it always returns float type! -> 2.0
 
 # [IMPORTANT]: exponent! to increase power
-exp = 2 ** 3 # 8
-print(exp) # 8
+exp = 2**3  # 8
+print(exp)  # 8
 
 # PEMDAS (Prioirty) with LR (left to right)
 # ()
@@ -18,9 +72,8 @@ print(exp) # 8
 # * / (always left one first)
 # + - (always left one first)
 
-print(3/3 + 3*3 - 3) # / * have the same priority so that left one "/" first. --> 7.0
-
-
+print(3 / 3 + 3 * 3 -
+      3)  # / * have the same priority so that left one "/" first. --> 7.0
 
 #  ======= Data Types: String (str), Integer (int), Float (float), Bloean (bool) ===
 print("======== Data type ===========")
@@ -50,7 +103,8 @@ False  # works!!!
 name_char = len(input("What is your name? "))
 
 # check type
-_type = type(name_char)  # IMPORTANT type() returns object type, not a String type.
+_type = type(
+    name_char)  # IMPORTANT type() returns object type, not a String type.
 print(_type)  # It cannot concatenated with String
 
 # if it is not the same type, conver it to the correct type.
