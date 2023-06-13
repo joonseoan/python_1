514682ed0d5e0a2ac286b69878d8c6e3ddde2d53
Joon <33235661+joonseoan@users.noreply.github.com>
2023-06-12 22:24:48 -0400

    challenge done

--- main.py
+++ main.py
@@ -3,6 +3,29 @@ print("")
 print("====================== Day6 =================")
 
 print("")
+print("------- while loop challenge --------")
+print("")
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_right():
+#     turn_left();
+#     turn_left();
+#     turn_left();
+#     move();
+
+# def runHurdle():
+#     turn_left();
+#     move();
+#     turn_right();
+#     turn_right();
+#     turn_left();
+    
+# while not at_goal():
+#     if front_is_clear(): move();
+#     else: runHurdle()
+
+print("")
 print("------- while loop -----------")
 print("")
 
