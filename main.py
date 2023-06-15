2f108b560dd2b3aa061a127732d920348695b88a
Joon <33235661+joonseoan@users.noreply.github.com>
2023-06-14 23:36:00 -0400

    done for challenge

--- main.py
+++ main.py
@@ -6,6 +6,31 @@ print("")
 print("------- while loop challenge --------")
 print("")
 
+# Challenge
+# Maze
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
+
+# def turn_right():
+#     turn_left();
+#     turn_left();
+#     turn_left();
+
+# It is for to prevent the infinite loop in some position.
+# while front_is_clear():
+#     move();
+# It is same as the one below
+# turn_left();
+              
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right();
+#         move();
+#     elif front_is_clear():
+#         move();
+#     else:
+#         turn_left();
+
+
 # [4] hurdle 4
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 
