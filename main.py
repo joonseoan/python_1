54f3aeaff8c5715450d97fcef5c5aa1008127a84
Joon <33235661+joonseoan@users.noreply.github.com>
2023-06-16 00:01:24 -0400

    added hangman step 1

--- main.py
+++ main.py
@@ -1,4 +1,37 @@
 print("")
+# ======================= Day 7 ==============================
+print("====================== Day 7 =================")
+
+print("")
+print("------- hangman step 1 --------")
+print("")
+
+# Review List
+# https://developers.google.com/edu/python/lists#for-and-in
+
+#Step 1 
+
+word_list = ["aardvark", "baboon", "camel"]
+
+#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+import random
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess = (input("guess a letter in the word: ")).lower()
+
+#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
+for ch in chosen_word:
+    if ch == guess:
+        print("right")
+    else:
+        print("wrong")
+
+
+print("")
 # ======================= Day 6 ==============================
 print("====================== Day6 =================")
 
