1ab31fe2aae87d08cd224f424d1ed1b97c467fdc
Joon <33235661+joonseoan@users.noreply.github.com>
2023-07-11 08:35:41 -0400

    Added Refactoring

--- main.py
+++ main.py
@@ -3,7 +3,37 @@ print("")
 print("====================== Day8 =================")
 
 print("")
-print("------- Chellnge - Cyphering and Decyphering part 1 --------")
+print("------- Chellnge - Cyphering and Decyphering part 2 (Refactoring) --------")
+print("")
+
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def caesar(input_text, shift_amount, input_direction):
+    result = ""
+
+    if input_direction == "decode":
+        shift_amount *= -1
+
+    for letter in input_text:
+        index = alphabet.index(letter)
+             
+        result += alphabet[index + shift_amount]
+
+    print(f"The {input_direction}d text is {result}")
+
+caesar(input_text=text, shift_amount=shift, input_direction=direction)
+
+print("")
+print("------- Chellnge - Cyphering and Decyphering part 1 (encode and decode) --------")
 print("")
 
 alphabet = [
