2df6a914c09bd1edc6538796049157ec21ec668a
Joon <33235661+joonseoan@users.noreply.github.com>
2023-09-06 13:53:26 -0400

    update teacher's solution for capstone

--- main.py
+++ main.py
@@ -9,6 +9,97 @@ print("")
 # project site
 # https://replit.com/@JoonAn1/blackjack-start#main.py
 
+# ----------------------- Teacher Solution --------------------------
+import random
+# from replit import clear
+# from art import logo
+
+def deal_card():
+    """ Returns a random card from the deck. """
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    return random.choice(cards)
+
+
+def compare(user_score, computer_score):
+    if user_score == computer_score:
+        return "Draw"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack"
+    elif user_score == 0:
+        return "Win with a Blackjack"
+    elif user_score > 21:
+        return "You went over. You lose"
+    elif computer_score > 21:
+        return "Oponent Went over. You win"
+    elif user_score > computer_score:
+        return "You win"
+    else:
+        return "You lose"
+
+
+def play_blackjack():
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    # [Important]: With one argument, it `stop` argument from 0
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    def calculate_score(cards):
+        """ Receive the current cards and return the calculated score"""
+        # Better way but need to learn "in"
+        # if sum(cards) == 21 and len(cards) == 2:
+        if 11 in cards and 10 in cards and len(cards) == 2:
+            return 0
+
+        if 11 in cards and sum(cards) > 21:
+            # IMPORTANT: we can remove the element by using element value
+            # IMPORTANT!!!!!: manipulate the outside variable with utilizing the parameters
+            cards.remove(11)
+            cards.append(1)
+
+        return sum(cards)
+
+    while not is_game_over:
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+
+        print(f"    Your cards: {user_cards}, current score: {user_score}")
+        print(f"    Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+            user_should_deal = input(
+                "Type 'y' to get anohter card, type 'n' to pass: ")
+            if user_should_deal.lower() == 'y':
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    # After while loop above
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"    your final hands: {user_cards}, and final score: {user_score}")
+    print(
+        f"    computer final hands: {computer_cards}, and final score: {computer_score}"
+    )
+
+    # A big question. How can we call user_score and computer_score defined in another while loop above?
+    print(compare(user_score, computer_score))
+
+
+while input("Do you want to play a game of Blackjack? Enter 'y' or 'no': "
+            ).lower() == "y":
+    # clear()
+    # print(logo)
+    play_blackjack()
+
+# ---------------- My solution ---------------------
 # import art
 import random
 from os import system
